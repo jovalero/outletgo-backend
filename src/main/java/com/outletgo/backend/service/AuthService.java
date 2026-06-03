f4343e0a82773ccb1548a5bd880421fbdfb13873
jovalero <jrvalero2000@gmail.com>
2026-06-03 12:19:54 -0300

    Add UUID import in AuthService

--- src/main/java/com/outletgo/backend/service/AuthService.java
+++ src/main/java/com/outletgo/backend/service/AuthService.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.UUID;
 
 @Service
 public class AuthService {
