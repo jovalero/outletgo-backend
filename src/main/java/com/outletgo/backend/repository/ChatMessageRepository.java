f36a08b6c47747a7b497b735306d9a6f88a2aa9f
jovalero <jrvalero2000@gmail.com>
2026-06-03 00:17:12 -0300

    Rename query method in ChatMessageRepository to match sentAt property

--- src/main/java/com/outletgo/backend/repository/ChatMessageRepository.java
+++ src/main/java/com/outletgo/backend/repository/ChatMessageRepository.java
@@ -9,5 +9,5 @@ import java.util.UUID;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, UUID> {
-    List<ChatMessage> findByStoreIdOrderBySendAtAsc(UUID storeId);
+    List<ChatMessage> findByStoreIdOrderBySentAtAsc(UUID storeId);
 }
