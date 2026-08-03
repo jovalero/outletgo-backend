ee4902450a5849d1831c6ca61b179faf9fb08670
jovalero <jrvalero22@gmail.com>
2026-08-03 12:40:24 -0300

    Fix: Explicitly map GetMapping in AdminBannerController

--- src/main/java/com/outletgo/backend/controller/AdminBannerController.java
+++ src/main/java/com/outletgo/backend/controller/AdminBannerController.java
@@ -26,7 +26,7 @@ public class AdminBannerController {
 
     private final BannerService bannerService;
 
-    @GetMapping
+    @GetMapping({"", "/"})
     public ResponseEntity<?> getBanners(
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "10") int size) {
