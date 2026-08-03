766b5f83b4dc027d81f9e71186646f0bdcebd3cf
jovalero <jrvalero22@gmail.com>
2026-08-03 14:10:28 -0300

    Fix: Add missing @Slf4j annotation to SellerController to resolve Maven build failure

--- src/main/java/com/outletgo/backend/controller/SellerController.java
+++ src/main/java/com/outletgo/backend/controller/SellerController.java
@@ -5,6 +5,7 @@ import com.outletgo.backend.entity.*;
 import com.outletgo.backend.repository.*;
 import io.jsonwebtoken.Claims;
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.*;
 import org.springframework.http.HttpStatus;
@@ -21,6 +22,7 @@ import java.util.stream.Collectors;
 @CrossOrigin
 @Transactional
 @RequestMapping("/api/seller")
+@Slf4j
 public class SellerController {
 
     @Autowired
