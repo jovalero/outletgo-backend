78fd1159a4ff7912796d271b94a39238ba22f0e8
jovalero <jrvalero22@gmail.com>
2026-08-05 02:42:22 -0300

    fix: import org.springframework.transaction.annotation.Transactional in AdminBannerController

--- src/main/java/com/outletgo/backend/controller/AdminBannerController.java
+++ src/main/java/com/outletgo/backend/controller/AdminBannerController.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
